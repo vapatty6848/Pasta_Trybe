785bb48019e36a5494126d67d57c13554a7690ac
vania patricia <vapatty@gmail.com>
2020-07-26 10:23:33 -0300

    Exercicio 04 - dado um valor definido numa variável, retorne positive se esse valor for positivo, negative se for negativo e zero caso contrário.

---
+++ exercises/3_4/exercises/4_1/exercicio_04.js
@@ -0,0 +1,9 @@
+var a = 0;
+
+if (a > 0){
+    console.log("O número é positivo   "+ a);
+} else if(a < 0) {
+    console.log("o número é negativo  "+ a);
+} else {
+    console.log(" o numero é zero");
+}
