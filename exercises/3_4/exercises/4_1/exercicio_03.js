24c6c8dec97520b734b52b023b0aab2e14c88bf7
vania patricia <vapatty@gmail.com>
2020-07-26 10:14:43 -0300

    exercicio 03 - mostrar o maior de 3 números

---
+++ exercises/3_4/exercises/4_1/exercicio_03.js
@@ -0,0 +1,14 @@
+var a = 3000;
+var b =2000;
+var c = 1200;
+
+if (a > b && a > c){
+    console.log("O número maior ou igual  é "+ a);
+} else if(b > a && b > c) {
+    console.log("o número maior é "+ b);
+} else if( a == b & b == c) {
+    console.log( " os números são iguais");
+} else {
+    console.log(" o numero " + c +  " É MAIOR");
+}
+
