92b4290a99a9b2183dd4733a47b5c287b9e2c16b
vania patricia <vapatty@gmail.com>
2020-07-26 11:35:45 -0300

    defiira três números em variáveis no seu começo e retorare true se as três forem  par. Caso contrário, ele retorna false.

---
+++ exercises/3_4/exercises/4_1/exercicio_08.js
@@ -0,0 +1,10 @@
+var a = 56;
+var b = 46;
+var c = 88;
+var isTrue = true;
+if ((a % 2 === 0) && (b % 2 === 0) && (c % 2 === 0)){
+    isTrue = true;
+} else {
+    isTrue = false;
+}
+console.log(isTrue);
