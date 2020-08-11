67ffc0319bbd0317ece8d3a375e2479dbed2395e
vania patricia <vapatty@gmail.com>
2020-08-11 18:32:30 -0300

    função add 3 parametros (objeto, a chave e o valor)

---
+++ 7_2/exercicio/7_2_01.js
@@ -0,0 +1,28 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+ function addTurno  ( ) {
+   lesson2.turno="manhã";
+ }
+
+ addTurno();
+ console.log(Object.entries(lesson2));
+
+
+/* Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
