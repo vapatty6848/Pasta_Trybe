582a26aaef3a4fa9009baa4bd0d192d30352916c
vania patricia <vapatty@gmail.com>
2020-08-11 18:46:03 -0300

    funçaõ que lista as keys do objeto

--- 7_2/exercicio/7_2_01.js
+++ 7_2/exercicio/7_2_01.js
@@ -1,3 +1,5 @@
+
+/* Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
 const lesson1 = {
   materia: 'Matemática',
   numeroEstudantes: 20,
@@ -23,6 +25,9 @@ const lesson3 = {
 
  addTurno();
  console.log(Object.entries(lesson2));
+/* Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro. */
 
-
-/* Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
+function listKeys (lesson) {
+  console.log(Object.keys(lesson));
+}
+let lesson = listKeys(lesson2);
