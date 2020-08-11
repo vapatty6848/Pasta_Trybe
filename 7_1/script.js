39284c041642fb78717c8b3e306255cc8ea454ce
vania patricia <vapatty@gmail.com>
2020-08-11 10:13:19 -0300

    exercicio 7.1/04 - falta colocar Go trybe

--- 7_1/script.js
+++
@@ -1,4 +0,0 @@
-let clickCount = 0;
-let textToDisplay = document.getElementById("text");
-
-document.getElementById("buttonCont") .addEventListener("click", () => textToDisplay.innerHTML = clickCount += 1);
