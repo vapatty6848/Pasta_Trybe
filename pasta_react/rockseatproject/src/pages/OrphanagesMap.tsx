5a5924dc7d5d96417305ec66b0ab90a779ff4a64
vapatty6848 <vapatty@gmail.com>
2020-10-13 07:35:06 -0300

    commit inicial do projeto Rockseat

---
+++ pasta_react/rockseatproject/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,30 @@
+mport React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+import '../style/pages/orphanage-map.css';
+
+function OrphanagesMap() {
+  return (
+  <>
+    <div id="page-map">
+      <aside>
+      <header>
+        <img src={mapMarkerImg} alt="Happy" />
+
+        <h2>Escolha um orfanato no mapa</h2>
+        <p>Muitas crianças estão esperando a sua visita</p>
+      </header>
+        <footer>
+          <strong>São Paulo</strong>
+        </footer>
+      </aside>
+      <Link to="" className="create-orphanage">
+        <FiPlus size = {32} color = "#fff" />
+      </Link>
+    </div>
+  </>
+  );
+}
+export default OrphanagesMap;
